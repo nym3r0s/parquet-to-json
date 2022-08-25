13bed47e93e04423004b5bc857fe70f4483da3c6
Gokul Srinivas <gokul.srinivas@glovoapp.com>
2022-08-25 14:43:08 +0200

    Add script to generate json files from parquet

---
+++ script.py
@@ -0,0 +1,26 @@
+from pyarrow.parquet import ParquetFile
+import pyarrow as pa
+import json
+
+pf = ParquetFile('test.parquet')
+partNum = 0
+
+
+def writeToFile(data, part):
+    fileName = 'import-part-' + str(part).zfill(6) + ".json"
+    with open(fileName, 'w') as f:
+        f.write(data)
+        f.close()
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            first_ten_rows = next(pf.iter_batches(batch_size=1000))
+            df = pa.Table.from_batches([first_ten_rows]).to_pandas()
+            jsonstr = df.to_json(orient='records')
+            writeToFile(jsonstr, partNum)
+            partNum += 1
+            input("done")
+        except StopIteration:
+            break
